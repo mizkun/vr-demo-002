07634ca9a0814037ddcbd21260a68fce05dc511a
Kyohei Mizutani <kyohei.mizutani@gmail.com>
2016-12-13 19:27:06 +0900

    feat: Add initial scene to select demo type.

---
+++ Assets/MyAssets/Scripts/GazeCanvasEvent.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GazeCanvasEvent : MonoBehaviour {
+
+	public Image ProgressBar;
+	public Image BackgroundImage;
+	public Text CanvasText;
+
+	public float speed;
+
+	private bool isGazed;
+
+	void Start () {
+		isGazed = false;	
+	}
+	
+	void Update () {
+		if (isGazed) {
+			Vector3 progress = ProgressBar.transform.localScale;
+			if (progress.x >= 1.0f) {
+				doSomething ();
+			} else {
+				progress.x += speed;
+				ProgressBar.transform.localScale = progress;
+			}
+		}
+	}
+
+	public void startGaze () {
+		isGazed = true;
+	}
+
+	public void endGaze () {
+		isGazed = false;
+		Vector3 progress = ProgressBar.transform.localScale;
+		progress.x = 0;
+		ProgressBar.transform.localScale = progress;
+	}
+
+	void doSomething () {
+		isGazed = false;
+		Debug.Log ("gazed");
+	}
+
+
+}
